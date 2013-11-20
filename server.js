7f24603a7d20f44cff767a00fb23e991f997a8cb
chrisdlangton <chrislangton84@gmail.com>
2013-11-20 20:25:44 +1100

    first commit

---
+++ server.js
@@ -0,0 +1,177 @@
+var express = require('express'),
+    app     = express(),
+    mysql   = require('mysql'),
+    connectionpool = mysql.createPool({
+        host     : 'localhost',
+        user     : 'test',
+        password : '',
+        database : 'test'
+    });
+app.get('/:table', function(req,res){
+    connectionpool.getConnection(function(err, connection) {
+        if (err) {
+            console.error('CONNECTION error: ',err);
+            res.statusCode = 503;
+            res.send({
+                result: 'error',
+                err:    err.code
+            });
+        } else {
+            connection.query('SELECT * FROM '+req.params.table+' ORDER BY id DESC LIMIT 20', req.params.id, function(err, rows) {
+                if (err) {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err:    err.code
+                    });
+                } else {
+                    res.send({
+                        result: 'success',
+                        err:    '',
+                        json:   rows,
+                        length: rows.length
+                    });
+                }
+                connection.release();
+            });
+        }
+    });
+});
+app.get('/:table/:id', function(req,res){
+    connectionpool.getConnection(function(err, connection) {
+        if (err) {
+            console.error('CONNECTION error: ',err);
+            res.statusCode = 503;
+            res.send({
+                result: 'error',
+                err:    err.code
+            });
+        } else {
+            connection.query('SELECT * FROM '+req.params.table+' WHERE id = ?', req.params.id, function(err, rows) {
+                if (err) {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err:    err.code
+                    });
+                } else {
+                    res.send({
+                        result: 'success',
+                        err:    '',
+                        id:     req.params.id,
+                        json:   rows[0],
+                        length: 1
+                    });
+                    connection.release();
+                }
+            });
+        }
+    });
+});
+app.post('/:table', function(req,res){
+    connectionpool.getConnection(function(err, connection) {
+        if (err) {
+            console.error('CONNECTION error: ',err);
+            res.statusCode = 503;
+            res.send({
+                result: 'error',
+                err:    err.code
+            });
+        } else {
+            connection.query('INSERT INTO '+req.params.table+' SET ?', req.body, function(err, result) {
+                if (err) {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err:    err.code
+                    });
+                } else {
+                    res.send({
+                        result: 'success',
+                        err:    '',
+                        id:     result.insertId
+                    });
+                }
+                connection.release();
+            });
+        }
+    });
+});
+app.put('/:table/:id', function(req,res){
+    connectionpool.getConnection(function(err, connection) {
+        if (err) {
+            console.error('CONNECTION error: ',err);
+            res.statusCode = 503;
+            res.send({
+                result: 'error',
+                err:    err.code
+            });
+        } else {
+            connection.query('UPDATE '+req.params.table+' SET ? WHERE id='+connection.escape(req.params.id), req.body, function(err) {
+                if (err) {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err:    err.code
+                    });
+                } else {
+                    connection.query('SELECT * FROM '+req.params.table+' WHERE id = ?', req.params.id, function(err, rows) {
+                        if (err) {
+                            console.error(err);
+                            res.statusCode = 500;
+                            res.send({
+                                result: 'error',
+                                err:    err.code
+                            });
+                        } else {
+                            res.send({
+                                result: 'success',
+                                err:    '',
+                                id:     req.params.id,
+                                json:   rows[0],
+                                length: 1
+                            });
+                        }
+                        connection.release();
+                    });
+                }
+            });
+        }
+    });
+});
+app.delete('/:table/:id', function(req,res){
+    connectionpool.getConnection(function(err, connection) {
+        if (err) {
+            console.error('CONNECTION error: ',err);
+            res.statusCode = 503;
+            res.send({
+                result: 'error',
+                err:    err.code
+            });
+        } else {
+            connection.query('DELETE FROM '+req.params.table+' WHERE id = ?', req.params.id, function(err) {
+                if (err) {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err:    err.code
+                    });
+                } else {
+                    res.send({
+                        result: 'success',
+                        err:    '',
+                        id:     req.params.id
+                    });
+                }
+                connection.release();
+            });
+        }
+    });
+});
+app.listen(3000);
+console.log('Rest Demo Listening on port 3000');
